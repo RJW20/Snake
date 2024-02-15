74173037521f7014087e6bd6d1d2680d8cbf1508
Robert Watts <robertjwatts84@outlook.com>
2024-02-15 13:50:54 +0000

    Edited length setting

--- settings.py
+++ settings.py
@@ -8,6 +8,6 @@ settings = {
     'grid_width': 2,
 
     #snake properties
-    'initial_length': 3     #note will be set to min(grid_size)//2 if larger than that to prevent some of the body starting outside the grid
+    'length': 3     #note will be set to min(grid_size)//2 if larger than that to prevent some of the body starting outside the grid
 
 }
