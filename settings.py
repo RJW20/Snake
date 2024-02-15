e3d1c37bf76f5a0a51dd98974795670c71f113ab
Robert Watts <robertjwatts84@outlook.com>
2024-02-15 18:57:31 +0000

    renamed gridline_width to block_padding

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ settings = {
     #grid properties
     'grid_size': (30, 30),  #(width, height)
     'block_width': 20,
-    'gridline_width': 1,
+    'block_padding': 1,
 
     #snake properties
     'length': 3     #note will be set to min(grid_size)//2 if larger than that to prevent some of the body starting outside the grid
