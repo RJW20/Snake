3d20e29d3bd5da8d0066979daf96691a453b6f9a
Robert Watts <robertjwatts84@outlook.com>
2024-02-15 19:03:38 +0000

    renamed gridline_width to block_padding

--- snake_app/grid.py
+++ snake_app/grid.py
@@ -3,10 +3,10 @@ from functools import cached_property
 class Grid:
     """Grid that the game in played on."""
 
-    def __init__(self, size, block_width, gridline_width):
+    def __init__(self, size, block_width, block_padding):
         self.size = size
         self.block_width = block_width
-        self.gridline_width = gridline_width
+        self.block_padding = block_padding
 
     @cached_property
     def longest_edge(self):
@@ -14,14 +14,14 @@ class Grid:
     
     @cached_property
     def board_size(self):
-        return tuple(self.gridline_width + self.size[i] * (self.block_width + self.gridline_width) for i in range(2))
+        return tuple(self.block_padding + self.size[i] * (self.block_width + self.block_padding) for i in range(2))
     
     @cached_property
     def conversion_table(self):
         """Conversion table from gridpoint to x,y coordinates."""
         table = []
         for i in range(self.longest_edge):
-            table.append(self.gridline_width + i * (self.block_width + self.gridline_width))
+            table.append(self.block_padding + i * (self.block_width + self.block_padding))
         return table
 
     def gridpoint_to_coordinates(self, grid_point):
