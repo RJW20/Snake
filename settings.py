3cabc5888930297ee814d2a6c81ef02e8d23dd40
Robert Watts <robertjwatts84@outlook.com>
2024-02-15 13:46:35 +0000

    Added settings

---
+++ settings.py
@@ -0,0 +1,13 @@
+
+
+settings = {
+
+    #grid properties
+    'grid_size': (10, 10),
+    'block_width': 30,
+    'grid_width': 2,
+
+    #snake properties
+    'initial_length': 3     #note will be set to min(grid_size)//2 if larger than that to prevent some of the body starting outside the grid
+
+}
